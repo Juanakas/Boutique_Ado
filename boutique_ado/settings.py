32fcb3e43ff0021ece956a96dc22a823fde552d7
Juanakas <juanclementeguerrero@outlook.com>
2026-05-23 17:30:55 +0100

    completed homepage header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -153,3 +153,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
